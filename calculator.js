ecda6e70b856b5d02c4c024cb05d677cc3ae477d
heastyfrost <heastfrostel@gmail.com>
2026-04-22 18:00:49 +0800

    Initial commit - added addTwoAndSeven function

---
+++ calculator.js
@@ -0,0 +1,12 @@
+// Function to add 2 and 7 (regular function syntax)
+function addTwoAndSeven() {
+    return 2 + 7;
+}
+
+// OR using arrow function syntax (choose one)
+// const addTwoAndSeven = () => {
+//     return 2 + 7;
+// }
+
+// Test the function
+console.log(addTwoAndSeven()); // This will output: 9
