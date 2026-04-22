2e096a9cfedfbd698c5d4ef5a8ea75268c066771
heastyfrost <heastfrostel@gmail.com>
2026-04-22 18:19:51 +0800

    step10

--- calculator.js
+++ calculator.js
@@ -5,3 +5,7 @@ function calculateSum(num1, num2) {
 console.log(calculateSum(10, 10));
 console.log(calculateSum(5, 5));
 console.log(calculateSum(2, 5));
+
+function calculateDifference(num1, num2) {
+    return num1 - num2;
+}
