769ae7968e33474eec730144c757a2793397dd6f
heastyfrost <heastfrostel@gmail.com>
2026-04-22 18:07:32 +0800

    Step 1

--- calculator.js
+++ calculator.js
@@ -1,12 +1,3 @@
-// Function to add 2 and 7 (regular function syntax)
 function addTwoAndSeven() {
-    return 2 + 7;
+    // return will be added in Step 2
 }
-
-// OR using arrow function syntax (choose one)
-// const addTwoAndSeven = () => {
-//     return 2 + 7;
-// }
-
-// Test the function
-console.log(addTwoAndSeven()); // This will output: 9
